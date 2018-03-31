287e0c2acf86d97a49e3fe9d518946cd21e41961
Zacki Zulfikar Fauzi <zackizf1204@gmail.com>
2018-03-31 12:30:15 +0700

    Main menu without background

---
+++ Assets/Script/MainMenu/Controller/ExitController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ExitController : MonoBehaviour {
+		// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	public void ClickExit(){
+		#if UNITY_STANDALONE
+		//Quit the application
+		Application.Quit();
+		#endif
+
+		//If we are running in the editor
+		#if UNITY_EDITOR
+		//Stop playing the scene
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+}
