327b4233c567f25e3f3ec3e93f160566dc91d3c5
Muhammad Fathur Rahman <mfathur2707@gmail.com>
2018-03-31 12:55:48 +0900

    Repeating Background

---
+++ Assets/Script/Game/Controller/scroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, 0);
+		GetComponent<Renderer> ().material.mainTextureOffset = offset;
+	}
+}
